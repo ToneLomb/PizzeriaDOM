7c534824feff98ccf55bcfe31b4846cab4182968
Kurnoth <arnaud.kohler@bbox.fr>
2023-10-17 15:06:01 +0200

    Add src with classes

---
+++ PizzeriaDOM/src/classes/Customer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaDOM.src.classes
+{
+    internal class Customer
+    {
+        private string _surname;
+        private string _firstName;
+        private string _address;
+        private string _telephoneNumber;
+        private DateTime _dateFirstOrder;
+        private int _purchaseNumber;
+
+        public Customer(string surname, string firstName, string address, string telephoneNumber, DateTime dateFirstOrder)
+        {
+            this._surname = surname;
+            this._firstName = firstName;
+            this._address = address;
+            this._telephoneNumber = telephoneNumber;
+            this._dateFirstOrder = dateFirstOrder;
+            this._purchaseNumber = 0;
+        }
+
+        public string Surname
+        {
+            get => _surname;
+            set => _surname = value;
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+
+        public string Adress
+        {
+            get => _address;
+            set => _address = value;
+        }
+
+        public string TelephoneNumber
+        {
+            get => _telephoneNumber;
+            set => _telephoneNumber = value;
+        }
+
+        public DateTime DateFirstOrder
+        {
+            get => _dateFirstOrder;
+            set => _dateFirstOrder = value;
+        }
+
+        public int PurchaseNumber
+        {
+            get { return _purchaseNumber; }
+            set => _purchaseNumber = value;
+        }
+    }
+}
---
+++ PizzeriaDOM/src/classes/Order.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaDOM.src.classes
+{
+    internal class Order
+    {
+        private int _ID;
+        private string _customerTelephoneNumber;
+        private double _priceOrder;
+        private string _state; // [Preparation, delivery, closed, lost]
+        private DateTime _dateOrder;
+        private List<Product> _product;
+
+        //DÃ©finir un certain prix selon type / size pizza
+
+        public Order(int ID, string customerTelephoneNumber, double priceOrder, string state, DateTime dateOrder, List<Product> product)
+        {
+            this._ID = ID;
+            this._customerTelephoneNumber = customerTelephoneNumber;
+            this._priceOrder = priceOrder;
+            this._state = state;
+            this._dateOrder = dateOrder;
+            this._product = product;
+        }
+
+        public int ID
+        {
+            get => _ID;
+            set => _ID = value;
+        }
+
+        public string CustomerTelephoneNumber
+        {
+            get => _customerTelephoneNumber;
+            set => _customerTelephoneNumber = value;
+        }
+
+        public double PriceOrder
+        {
+            get => _priceOrder;
+            set => _priceOrder = value;
+        }
+
+        public string State
+        {
+            get => _state;
+            set => _state = value;
+        }
+
+        public DateTime DateOrder
+        {
+            get => _dateOrder;
+            set => _dateOrder = value;
+        }
+
+        public List<Product> ProductList
+        {
+            get => _product;
+            set => _product = value;
+        }
+
+        public class Product
+        {
+            private string _size;
+            private string _type;
+            private int _price;
+
+            public Product(string size, string type, int price)
+            {
+                this._size = size;
+                this._type = type;
+                this._price = price;
+            }
+
+            public string Size
+            {
+                get => _size;
+                set => _size = value;
+            }
+
+            public string Type
+            {
+                get => _type;
+                set => _type = value;
+            }
+
+            public int Price
+            {
+                get => _price;
+                set => _price = value;
+            }
+        }
+    }
+}
