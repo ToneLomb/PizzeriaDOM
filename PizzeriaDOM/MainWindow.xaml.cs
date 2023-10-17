74cd4d45bb64efc2d36070533b6e4c82c62c32b7
lucas <lucas@PC-DE-LUCAS>
2023-10-17 13:47:25 +0200

    Ajoutez des fichiers projet.

---
+++ PizzeriaDOM/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzeriaDOM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CC.Content = new PizzeriaDOM.Page1();
+        }
+
+        private void ToggleButtonClick(ToggleButton selectedButton)
+        {
+            // Parcourir tous les ToggleButton dans la NavBar
+            foreach (var child in NavBar.Children)
+            {   
+                // Si c'est un ToggleButton
+                if (child is ToggleButton toggleButton)
+                {
+                    // On le désactive (ils seront donc tous désactivés)
+                    toggleButton.IsChecked = false;
+                }
+            }
+
+            // Sélectionner le bouton actuel et l'active
+            selectedButton.IsChecked = true;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            CC.Content = new PizzeriaDOM.Page1();
+            ToggleButtonClick(button1);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            CC.Content = new PizzeriaDOM.Page2();
+            ToggleButtonClick(button2);
+        }
+    }
+}
