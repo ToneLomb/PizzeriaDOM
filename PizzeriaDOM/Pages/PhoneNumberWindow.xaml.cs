a27dabec9961d082d2d2d0668008775e8653f491
ToneLomb <lucas.brancolini@efrei.net>
2023-10-18 22:46:08 +0200

    Display of the customer info on the TakeOrder page

--- PizzeriaDOM/Pages/PhoneNumberWindow.xaml.cs
+++ PizzeriaDOM/Pages/PhoneNumberWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,13 +28,31 @@ namespace PizzeriaDOM.Pages
             InitializeComponent();
         }
 
+        private bool isNumber;
 
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Return)
             {
-                PhoneNumber = TextPhoneNumber.Text;
-                this.Close();
+                isNumber = true;
+                //Permet de check si toute la chaîne de caractère contient uniquement des digit
+                foreach(char c in TextPhoneNumber.Text)
+                {
+                    if (!Char.IsDigit(c))
+                    {
+                        isNumber = false;
+                    }
+                }
+                if (isNumber)
+                {
+                    PhoneNumber = TextPhoneNumber.Text;
+                    this.Close();
+                }
+                else
+                {
+                    Error.Content = "Only digit are allowed !";
+                }
+                
             }
         }
     }
--- PizzeriaDOM/Pages/RegisterCustomerWindow.xaml.cs
+++ PizzeriaDOM/Pages/RegisterCustomerWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace PizzeriaDOM.Pages
         public Customer NewCustomer { get; private set; }
         private void Register_Click(object sender, RoutedEventArgs e)
         {
+            //List des champs à vérifier
             List<TextBox> fields = new List<TextBox>
             {
                 Surname,
@@ -37,23 +38,26 @@ namespace PizzeriaDOM.Pages
                 Adress,
                 Phone
             };
-
+            
+            //Parcours des valeurs des champs
             foreach (TextBox textBox in fields)
             {
+                //Si une seule est vide
                 if (string.IsNullOrEmpty(textBox.Text))
                 {
+                    //Un champ est vide et on sort de la boucle
                     isEmpty = true;
-                    break; // Si un TextBox est vide, sortez de la boucle.
+                    break; 
                 }
             }
-
+            //Annonce de l'erreur
             if (isEmpty)
             {
                 Error.Content = "Fields cannot be empty !";
             }
-            else
+            else //Sinon on créer le customer, l'ajoute dans une liste pour pouvoir l'écrire dans notre fichier BDD, et on ferme la popup
             {
-                Customer customer = new Customer(Surname.Text, Firstname.Text, Adress.Text, Phone.Text, DateTime.Now);
+                Customer customer = new Customer(Surname.Text, Firstname.Text, Adress.Text, Phone.Text, DateTime.Now); 
                 NewCustomer = customer;
                 List<Object> newCustomer = new List<Object>();
                 newCustomer.Add(customer);
