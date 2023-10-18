0e4f754c4468d359abddfad6f8af984ee7f03f18
Kurnoth <arnaud.kohler@bbox.fr>
2023-10-18 14:25:38 +0200

    Fix functions

--- PizzeriaDOM/src/classes/Customer.cs
+++ PizzeriaDOM/src/classes/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PizzeriaDOM.src.classes
 {
-    internal class Customer
+    public class Customer
     {
         private string _surname;
         private string _firstName;
