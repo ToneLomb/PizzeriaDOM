234b839634d5a8add90195e0cbd9250159ddc416
Kurnoth <arnaud.kohler@bbox.fr>
2023-10-18 11:36:31 +0200

    Add function to seach customer and fix IOFile fucntions

---
+++ PizzeriaDOM/src/functions/checkFunctions.cs
@@ -0,0 +1,26 @@
+﻿using PizzeriaDOM.src.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaDOM.src.functions
+{
+    public class checkFunctions
+    {
+        public static Boolean customerExist(String telephoneNumber)
+        {
+            List<Customer> listFromFile = IOFile.ReadFromFile<Customer>("Customer");
+            Customer customerFound = listFromFile.FirstOrDefault(c => c.TelephoneNumber == telephoneNumber);
+
+            if (customerFound != null)
+            {
+                return true;
+            }
+            else { return false; }
+
+        }
+
+    }
+}
