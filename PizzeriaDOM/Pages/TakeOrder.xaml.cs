45e346a1fb5a6ce5f65825b3d48ec43dcb0e6030
ToneLomb <lucas.brancolini@efrei.net>
2023-10-19 19:27:18 +0200

    ADDED Filter Box + Quantity + Fixed Product bug

--- PizzeriaDOM/Pages/TakeOrder.xaml.cs
+++ PizzeriaDOM/Pages/TakeOrder.xaml.cs
@@ -48,7 +48,7 @@ namespace PizzeriaDOM.Pages
         {
             //On récupère le numéro de téléphone du client
             PhoneNumberWindow phoneNumberWindow = new PhoneNumberWindow();
-            phoneNumberWindow.ShowDialog(); 
+            phoneNumberWindow.ShowDialog();
             string phoneNumber = phoneNumberWindow.PhoneNumber;
             phoneNumberWindow.Close();
 
@@ -56,7 +56,7 @@ namespace PizzeriaDOM.Pages
             this.customer = checkFunctions.customerExist(phoneNumber);
 
             //Sinon on appelle la fenêtre de formulaire pour l'enregistrer
-            if(customer == null)
+            if (customer == null)
             {
                 RegisterCustomerWindow registerCustomerWindow = new RegisterCustomerWindow();
                 registerCustomerWindow.ShowDialog();
@@ -66,8 +66,8 @@ namespace PizzeriaDOM.Pages
 
             //Affichage du nom du client
             CustomerName.Content = "Customer : " + customer.Surname + " " + customer.FirstName;
-            
-            
+
+
         }
 
         private void createProduct_Click(object sender, RoutedEventArgs e)
@@ -75,12 +75,144 @@ namespace PizzeriaDOM.Pages
             //On crée une nouvelle instance du panel
             ProductPanel product = new ProductPanel
             {
-                ID = productIdCounter
+                ID = productIdCounter,
+                sizeGroup = "SizeGroup" + productIdCounter,
+                productName = "ProductName" + productIdCounter
+
             };
+
 
             // Ajout à la collection
             Products.Add(product);
+            //Set du groupName unique des radioBouttons
             productIdCounter++;
+        }
+
+        //Les deux méthodes ci dessous permettent de mettre le placeholder "Search ..."
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox search = (TextBox)sender;
+            if (search.Text == "Search ...")
+            {
+                search.Text = string.Empty;
+            }
+        }
+
+        private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox search = (TextBox)sender;
+            if (search.Text == string.Empty)
+            {
+                search.Text = "Search ...";
+            }
+        }
+
+        //Si pizza ou boisson checked, alors on met à jour le booléen de l'élément correspondant
+        private void Boisson_Checked(object sender, RoutedEventArgs e)
+        {
+            ProductPanel product = (ProductPanel)(sender as RadioButton).DataContext;
+            product.IsBoissonSelected = true;
+        }
+
+        private void Pizza_Checked(object sender, RoutedEventArgs e)
+        {
+            ProductPanel product = (ProductPanel)(sender as RadioButton).DataContext;
+            product.IsPizzaSelected = true;
+        }
+
+        private void Boisson_UnChecked(object sender, RoutedEventArgs e)
+        {
+            ProductPanel product = (ProductPanel)(sender as RadioButton).DataContext;
+            product.IsBoissonSelected = false;
+        }
+
+        private void Pizza_UnChecked(object sender, RoutedEventArgs e)
+        {
+            ProductPanel product = (ProductPanel)(sender as RadioButton).DataContext;
+            product.IsPizzaSelected = false;
+        }
+
+        private void SearchBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            //On récupère la TextBox sur laquelle on a cliqué
+            TextBox search = (TextBox)sender;
+
+            //On récupère l'élément parent pour pouvoir accéder à la listBox et les suggestions de Produits
+            StackPanel parentStackPanel = (StackPanel)search.Parent;
+            ListBox suggestionList = (ListBox)parentStackPanel.FindName("suggestionList");
+            Popup suggestionPopup = (Popup)parentStackPanel.FindName("suggestionPopup");
+
+            //On récupère également le ProductPanel associé et en fonction du choix du client on charge la BDD associée aux produits
+            ProductPanel productPanel = (ProductPanel)parentStackPanel.DataContext;
+            string databaseToLoad = productPanel.IsPizzaSelected ? "Pizza" : "Drinks";
+ 
+            List<Order.Product> suggestions = IOFile.ReadFromFile<Order.Product>(databaseToLoad);
+
+            //On récupère les noms des Produits
+            List<string> productsName = new List<string>();
+            foreach(Order.Product product in suggestions)
+            {
+                productsName.Add(product.Name);
+            }
+
+            //Filtre en fonction de ce que l'utilisateur tape
+            List<string> filteredSuggestions = productsName
+            .Where(product => product.ToLower().Contains(search.Text.ToLower()))
+            .ToList();
+
+            //Mettre à jour la ListBox avec les suggestions filtrées
+            suggestionList.ItemsSource = filteredSuggestions;
+            suggestionPopup.IsOpen = productsName.Count > 0;
+        }
+
+        private void suggestionList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //On récupère le StackPanel qui est le grand parent de la list box
+            ListBox suggestionList = (ListBox)sender;
+            Popup popup = (Popup)suggestionList.Parent;
+            StackPanel parentStackPanel = (StackPanel)popup.Parent;
+
+            //On récupère le produit associé 
+            ProductPanel product = (ProductPanel)parentStackPanel.DataContext;
+
+            //On enregistre la selection du client dans le produit
+            if (suggestionList.SelectedItem != null)
+            {
+                string selectedSuggestion = suggestionList.SelectedItem.ToString();
+                product.selectedProduct = selectedSuggestion;
+
+            }
+
+            TextBox search = (TextBox)parentStackPanel.FindName("SearchBox");
+            search.Text = product.selectedProduct;
+        }
+
+        private void Decrease_Click(object sender, RoutedEventArgs e)
+        {
+            Button decrease = (Button)sender;
+            StackPanel parentStackPanel = (StackPanel)decrease.Parent;
+            StackPanel grandParentStackPanel = (StackPanel)parentStackPanel.Parent;
+
+            TextBlock quantity = (TextBlock)grandParentStackPanel.FindName("Quantity");
+            int value = int.Parse(quantity.Text);
+            Trace.WriteLine(value);
+            if (value > 1) {
+                value--;
+            }
+            quantity.Text = value.ToString();
+
+        }
+
+        private void Increment_Click(object sender, RoutedEventArgs e)
+        {
+            Button increase = (Button)sender;
+            StackPanel parentStackPanel = (StackPanel)increase.Parent;
+            StackPanel grandParentStackPanel = (StackPanel)parentStackPanel.Parent;
+
+            TextBlock quantity = (TextBlock)grandParentStackPanel.FindName("Quantity");
+            int value = int.Parse(quantity.Text);
+            value++;
+            quantity.Text = value.ToString();
         }
     }
 }
--- PizzeriaDOM/src/classes/ProductPanel.cs
+++ PizzeriaDOM/src/classes/ProductPanel.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace PizzeriaDOM.src.classes
 {
-    public class ProductPanel
+    public class ProductPanel : INotifyPropertyChanged
     {
         public int ID {  get; set; }
-        public bool IsBoissonSelected { get; set; }
-        public bool IsPizzaSelected { get; set; }
+
+        public string sizeGroup { get; set; }
+
+        public string productName { get; set; }
+
+        public string selectedProduct { get; set; }
+
+        public int quantity { get; set; }   
+
+        //Déf du booléen
+        private bool isBoissonSelected;
+
+        //Définition du Getter/Setter
+        public bool IsBoissonSelected
+        {
+            get { return isBoissonSelected; }
+            set
+            {
+                //Si l'ancienne valeur est effectivement différente de la nouvelle
+                if (isBoissonSelected != value)
+                {   
+                    //Alors on la met à jour et on appelle la méthode OnPropertyChanged en lui indiquant le nom de la propriété qui a changé
+                    isBoissonSelected = value;
+                    OnPropertyChanged(nameof(IsBoissonSelected));
+                }
+            }
+        }
+
+        //Même qu'au dessus
+        private bool isPizzaSelected;
+        public bool IsPizzaSelected
+        {
+            get { return isPizzaSelected; }
+            set
+            {
+                if (isPizzaSelected != value)
+                {
+                    isPizzaSelected = value;
+                    OnPropertyChanged(nameof(IsPizzaSelected));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Permet de Bind avec le XMAL
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
