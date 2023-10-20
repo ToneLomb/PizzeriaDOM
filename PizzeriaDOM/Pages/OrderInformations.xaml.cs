bf6b8bbef9ecd5e71c44db360b85fb85a6b34255
ludovic <ludovic.liu.chi.pioa@efrei.net>
2023-10-20 15:49:20 +0200

    Added logic to display order informations dynamically (WIP)

--- PizzeriaDOM/Pages/OrderInformations.xaml.cs
+++ PizzeriaDOM/Pages/OrderInformations.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PizzeriaDOM.src.classes;
+using PizzeriaDOM.src.functions;
 
 namespace PizzeriaDOM.Pages
 {
@@ -20,9 +22,51 @@ namespace PizzeriaDOM.Pages
     /// </summary>
     public partial class OrderInformations : UserControl
     {
+        List<Order> orderList = new List<Order>();
+
         public OrderInformations()
         {
             InitializeComponent();
+
+            updateOrderList();
+        }
+
+        public void updateOrderList()
+        {
+            orderList = IOFile.ReadFromFile<Order>("Order");
+
+            foreach (Order order in orderList)
+            {
+                ComboID.Items.Add(new ComboBoxItem() { Content = order.ID });
+            }
+        }
+
+        private void ComboID_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            updateOrderList();
+
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+
+            //convert selectItem to an int
+            int selectedID = Convert.ToInt32(selectedItem.Content);
+
+            //get the order with the selected ID
+            Order selectedOrder = orderList.Find(order => order.ID == selectedID);
+
+            CustomerNumberTextBlock.Text = selectedOrder.CustomerTelephoneNumber;
+
+            TotalPriceTextBlock.Text = selectedOrder.PriceOrder.ToString();
+
+            StatusTextBlock.Text = selectedOrder.State;
+            //add changing color depending on the status ?
+
+            DateTextblock.Text = selectedOrder.DateOrder.ToString();
+
+            List<Order.Product> productList = selectedOrder.ProductList;
+
+            //display the list of product in the order
+            //TODO
         }
     }
 }
